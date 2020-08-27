519eebc28dc20e4867fbf7ce1fd12d88a914c1bf
Tristan Pacba <tpacba@live.com>
2020-08-26 23:01:08 -0400

    finished api routes and html routes and server.js, installed dependencies

---
+++ routes/apiRoutes.js
@@ -0,0 +1,23 @@
+var notesData = require("../db/db.json");
+
+module.exports = function(app) {
+    app.get("/api/notes", function(req, res) {
+        res.json(notesData);
+    })
+
+    app.post("/api/notes", function(req, res) {
+        notesData.push(req.body);
+        res.json(true)
+    })
+
+    app.delete("/api/notes/:id", function(req, res) {
+        var chosen = req.params.id;
+
+        for (var i = 0; i < notesData.length; i++) {
+            if (chosen === notesData[i].id) {
+                notesData.splice(i, 1);
+                res.json(notesData[i]);
+            }
+        }
+    })
+}
